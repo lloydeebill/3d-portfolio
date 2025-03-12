15082553a3f2b2cdeddfcc5563fa6c27aa6afc5b
lloydeebill <lloydbill.lariosa@gmail.com>
2025-03-12 18:47:14 +0800

    initial commits

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// src/App.jsx
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, 3D Portfolio!</h1>
+      <p>Welcome to your Vite + React project.</p>
+    </div>
+  );
+};
+
+export default App;
